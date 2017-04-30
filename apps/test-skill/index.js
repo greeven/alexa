7cbc04010962b6fb31f70a06b4c264b48379107e
greeven <greeven1@gmail.com>
2017-04-30 19:38:27 +0200

    Piep einmal Intent.

--- apps/test-skill/index.js
+++ apps/test-skill/index.js
@@ -18,29 +18,63 @@ app.error = function( exception, request, response ) {
 };
 
 app.intent('sayNumber',
-  {
-    "slots":{"number":"NUMBER"}
+{
+	"slots":{"number":"NUMBER"}
 	,"utterances":[ 
-		"sag die Nummer {1-100|number}"]
-  },
-  function(request,response) {
-    var number = request.slot('number');
-    response.say("Der große Meister fragte nach der Nummer " + number);
-  }
+	"sag die Nummer {1-100|number}"]
+},
+function(request,response) {
+	console.log('Request: ', request)
+	console.log('Response: ', response)
+
+	var number = request.slot('number');
+	response.say("Der große Meister fragte nach der Nummer " + number);
+}
 );
 
 app.intent('anotherNumber',
-  {
-    "slots":{"numberA":"NUMBER", "numberB":"NUMBER"}
+{
+	"slots":{"numberA":"NUMBER", "numberB":"NUMBER"}
 	,"utterances":[ 
-		"nummer aus {1-100|numberA} und {1-100|numberB}"]
-  },
-  function(request,response) {
-  	console.log('hey yo')
-    var numberA = request.slot('numberA');
-    var numberB = request.slot('numberB');
-    response.say("Die glorreiche Nummer ist " + numberA + "" + numberB + ". Cool, ne?");
-  }
+	"nummer aus {1-100|numberA} und {1-100|numberB}"]
+},
+function(request,response) {
+	console.log('hey yo')
+	var numberA = request.slot('numberA');
+	var numberB = request.slot('numberB');
+	response.say("Die glorreiche Nummer ist " + numberA + "" + numberB + ". Cool, ne?");
+}
 );
+
+app.intent("piep",
+{ 
+	"utterances":["piep ein mal"]
+},
+function(request,response) {
+	return new Promise(function(resolve,reject) {
+
+		wait(5000);
+
+		if (true) {
+			resolve();
+		}
+		else {
+			reject(Error("It broke"));
+		}
+	}).then(function(d){
+		response.say('piep');
+	});
+});
+
+function wait(ms){
+
+	var start = new Date().getTime();
+
+	var end = start;
+
+	while(end < start + ms) {
+		end = new Date().getTime();
+	}
+}
 
 module.exports = app;
